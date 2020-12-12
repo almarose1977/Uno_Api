aae7b60df019e3f8a606ef6af8bae9ca518a0357
Britta Schmitt <britta.schmitt@edu.campus02.at>
2020-12-12 09:22:21 +0100

    toUpperCase bei spielernamenCheck

--- uno.js
+++ uno.js
@@ -14,7 +14,10 @@ let punkte;             // Punktestand des Spielers
 
 let spielerNamenArray = [];     // Spielernamen Array erstellen
 let spielerIndex;
-let mitspieler = [];
+
+let Spielerinnen;               // = result.Players
+
+let Karte = {};
 
 // Div-Namen der Handkarten der Spieler, um die Handkarten nachher auszuteilen
 let handkartenDivNames = [];
@@ -22,86 +25,72 @@ let handkartenDivNames = [];
 // Dictionary/Map, um die Spielernamen-Divs mit den Spielernamen zu matchen --> drawCard: hier hab ich nur die Spielernamen zur Verfügung, brauch aber die entsprechende Id dazu
 let dictionary = {};
 
-//
-// ++++++++++++++++++++++++++ für Testzwecke geschrieben +++++++++++++++++++++++ später wieder löschen
-// nur für Testzwecke, damit man nicht ständig die Namen eingeben muss
-// let name1 = "Annie";
-// let name2 = "Brandi";
-// let name3 = "Annie";
-// let name4 = "Debbie";
-// ++++++++++++++++++++++++++ für Testzwecke geschrieben +++++++++++++++++++++++ später wieder löschen
-//
-
-
 // Array der Spieler-Namen-Divs --> um den Score zuzuweisen
 let divNamesScore = [];
-divNamesScore.push(document.getElementById("score-nord").id);
-divNamesScore.push(document.getElementById("score-ost").id);
-divNamesScore.push(document.getElementById("score-sued").id);
-divNamesScore.push(document.getElementById("score-west").id);
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ++++++++++++++++++++++++++ für Testzwecke geschrieben +++++++++++++++++++++++ später wieder löschen
+// nur für Testzwecke, damit man nicht ständig die Namen eingeben muss
+let name1 = "Annie";
+let name2 = "Brandi";
+let name3 = "Charly";
+let name4 = "Debbie";
+// ++++++++++++++++++++++++++ für Testzwecke geschrieben +++++++++++++++++++++++ später wieder löschen
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
 
-// Objekt Spielerin
-let Spielerinnen = {};
-
-// Objekt Karte
-let Karte = {};
 
 
 $('#playerNames').modal() // mit diesem aufruf wird der Inhalt des Modalen Dialogs angezeigt
 
 document.getElementById('playerNamesForm').addEventListener('submit', function (evt) {
-
+    
     evt.preventDefault(); // verhindert das Abschicken des post requests vom submit und damit das neuladen der seite (wollen nicht, dass beim uno die seite neu geladen wird)
     // hier kommt der code hin, der anstelle des post requests passieren soll
-    // die eingegebenen namen müssen in ein string array gespeichert werden, das ich dann der POST api/Game/Start übergeben
-
-
+    
+    
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // ++++++++++++++++++++++++++ für Testzwecke auskommentiert +++++++++++++++++++++++
-    let name1 = document.getElementById("player1_id").value;
-    let name2 = document.getElementById("player2_id").value;
-    let name3 = document.getElementById("player3_id").value;
-    let name4 = document.getElementById("player4_id").value;
+    // let name1 = document.getElementById("player1_id").value.toUpperCase();
+    // let name2 = document.getElementById("player2_id").value.toUpperCase();
+    // let name3 = document.getElementById("player3_id").value.toUpperCase();
+    // let name4 = document.getElementById("player4_id").value.toUpperCase();
+    
     // ++++++++++++++++++++++++++ für Testzwecke auskommentiert +++++++++++++++++++++++
-
+    
     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     // ++++++++++++++++++++++++++ für Testzwecke geschrieben +++++++++++++++++++++++ später wieder löschen
-    // document.getElementById("player1_id").value = name1;
-    // document.getElementById("player2_id").value = name2;
-    // document.getElementById("player3_id").value = name3;
-    // document.getElementById("player4_id").value = name4;
+    document.getElementById("player1_id").value = name1;
+    document.getElementById("player2_id").value = name2;
+    document.getElementById("player3_id").value = name3;
+    document.getElementById("player4_id").value = name4;
     // ++++++++++++++++++++++++++ für Testzwecke geschrieben +++++++++++++++++++++++ später wieder löschen
     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~++
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Spielernamen Vergleich
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~++
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     spielerNamenArray.push(name1, name2, name3, name4);
-
+    
     let checkedSpielerArray = spielerNamenArray.filter(function (name, index, array) {
         return index === array.indexOf(name);
     })
     if (checkedSpielerArray.length < 4) {
-        console.log(checkedSpielerArray)
         alert("Bitte gib 4 eindeutige Namen ein! Danke! :)")
     }
     else {
-
         
+        spielerNamenArray = checkedSpielerArray;
+
         // Dictionary befüllen: name wird der div.id zugewiesen
         dictionary[name1] = document.getElementById("sf-nord").id;
         dictionary[name2] = document.getElementById("sf-ost").id;
         dictionary[name3] = document.getElementById("sf-sued").id;
         dictionary[name4] = document.getElementById("sf-west").id;
-
-
-        // Handkarten-DIV-Namen (sf-nord, sf-ost, sf-sued, sf-west)
-        handkartenDivNames.push(document.getElementById("sf-nord").id);
-        handkartenDivNames.push(document.getElementById("sf-ost").id);
-        handkartenDivNames.push(document.getElementById("sf-sued").id);
-        handkartenDivNames.push(document.getElementById("sf-west").id);
+      
 
         // Spielernamen den entsprechenden sn-Elementen zuweisen
         document.getElementById("sn-nord").innerText = name1;
@@ -146,6 +135,9 @@ async function spielfeldLaden() {
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         gameId = result.Id;
 
+        // SPIELERINNEN 
+        Spielerinnen = result.Players;
+
         // AKTUELLER SPIELER
         aktuellerSpieler = result.NextPlayer;
         let p = document.createElement("p");
@@ -154,46 +146,34 @@ async function spielfeldLaden() {
         document.getElementById("activePlayer").appendChild(p);
 
         // STARTKARTE 
-        // let topcard = result.TopCard.Color + result.TopCard.Value;
-        // console.log("topcard", topcard);
         topCard = result.TopCard;
         let startkarte = document.getElementById("sf-mitte_ablagestapel");      // Startkarte dem entsprechenden DIV zuweisen
-        let img = new Image();
+        let img = document.createElement("img");
         img.src = "images/cards/" + topCard.Color + topCard.Value + ".png";
+        img.id = "startkarte";
         img.height = 150;
         startkarte.appendChild(img);
         startkarte = topCard;
 
-        // SPIELERINNEN 
-        Spielerinnen = result.Players;
+        // HANDKARTEN
+        // Handkarten-DIV-Namen (sf-nord, sf-ost, sf-sued, sf-west)
+        handkartenDivNames.push(document.getElementById("sf-nord").id);
+        handkartenDivNames.push(document.getElementById("sf-ost").id);
+        handkartenDivNames.push(document.getElementById("sf-sued").id);
+        handkartenDivNames.push(document.getElementById("sf-west").id);
 
-        mitspieler.push(Spielerinnen[0], Spielerinnen[1], Spielerinnen[2], Spielerinnen[3]);
 
-        // ZUM TESTEN
-        console.log("Mitspieler: ", mitspieler);
-        // console.log("Spielerin 1: ", Spielerinnen[0]);
-        // console.log("Spielerinnen-Name: ", Spielerinnen[0].Player);
-        // console.log("Handkarten der Spielerin 1: ", Spielerinnen[0].Score);
-        // console.log("Spielerinnen: ", Spielerinnen);
-        // console.log("Karte 1 der Spielerin 2: ", Spielerinnen[1].Cards[0]);
+        for (let i = 0; i < handkartenDivNames.length; i++) {               
 
-        // Karte.Color = result.Color;
-        // Karte.Text = result.Text;
-        // Karte.Value = result.Value;
-        // Karte.Score = result.Score;
-
-        // die Handkarten aller Spieler austeilen
-        for (let i = 0; i < handkartenDivNames.length; i++) {               // jedem Spieler
-
-            for (let j = 0; j < 7; j++) {                                   // werden 7 Karten zugeteilt
+            for (let j = 0; j < 7; j++) {                                   
                 let kartenDiv = document.createElement("div");              // jede Karte wird zu einem eigenen div
                 kartenDiv.setAttribute("class", "Handkarten_" + handkartenDivNames[i])   // Class Attribut, falls wir es brauchen
                 kartenDiv.id = "HK_Div_" + handkartenDivNames[i] + j;
                 //kartenDiv.setAttribute("onclick", "getId(this.id)");
-                let img = new Image();                                      // jeder Karte wird ein Bild zugewiesen
+                let img = document.createElement("img");                                     // jeder Karte wird ein Bild zugewiesen
                 img.src = "images/cards/" + Spielerinnen[i].Cards[j].Color + Spielerinnen[i].Cards[j].Value + ".png";
                 img.height = 90;
-                img.setAttribute("id", "HK_" + handkartenDivNames[i] + j);  // jedes Karten-Image hat eine id
+                img.id = "HK_" + handkartenDivNames[i] + j;  // jedes Karten-Image hat eine id
                 img.setAttribute("onclick", "playCard()");    // beim Klick auf die Karte wird die Funktion playCard() aufgerufen
                 kartenDiv.appendChild(img);
                 document.getElementById(handkartenDivNames[i]).appendChild(kartenDiv);
@@ -201,6 +181,11 @@ async function spielfeldLaden() {
         }
 
         // SCORE den einzelnen Spielern zuweisen
+        divNamesScore.push(document.getElementById("score-nord").id);
+        divNamesScore.push(document.getElementById("score-ost").id);
+        divNamesScore.push(document.getElementById("score-sued").id);
+        divNamesScore.push(document.getElementById("score-west").id);
+
         for (let i = 0; i < divNamesScore.length; i++) {
 
             punkte = document.createElement("p");
@@ -208,7 +193,9 @@ async function spielfeldLaden() {
             punkte.innerText = Spielerinnen[i].Score;
             document.getElementById(divNamesScore[i]).appendChild(punkte);
         }
-    } else {
+    } 
+    // if response not ok:
+    else {
         alert("HTTP-Error: " + response.status);
     }
 };
@@ -255,9 +242,9 @@ async function drawCard() {
 
         // Score der Spieler aktualisieren
         spielerIndex = spielerNamenArray.indexOf(aktuellerSpieler);
-        punkte = mitspieler[spielerIndex].Score;
-        mitspieler[spielerIndex].Score = punkte + result.Card.Score;
-        document.getElementById("punkteId_" + spielerIndex).innerText = String(mitspieler[spielerIndex].Score);
+        punkte = Spielerinnen[spielerIndex].Score;
+        Spielerinnen[spielerIndex].Score = punkte + result.Card.Score;
+        document.getElementById("punkteId_" + spielerIndex).innerText = String(Spielerinnen[spielerIndex].Score);
 
         // nächsten Spieler zum aktuellen Spieler machen
         aktuellerSpieler = result.NextPlayer;
